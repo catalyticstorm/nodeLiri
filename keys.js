54a778ab295b645f1af1582ed9c25a1c49e904df
catalyticstorm <32317951+catalyticstorm@users.noreply.github.com>
2018-02-26 19:50:21 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+//Let Us Know This Loaded
+console.log("Key Document Loaded.");
+
+// Twitter API
+// You're gonna have to put your own API key and secret here. For security reasons, I cannot have a Twitter account.
+
+
+// Spotify API
+
+
+// Request/OMDB API
+
+// DotEnv API
